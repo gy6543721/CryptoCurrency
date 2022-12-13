5bd573f18e122133e1d907d6981243b2b2f5ce01
gy6543721 <gy6543721@yahoo.com.tw>
2022-12-13 17:46:11 +0900

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Macbook Air M1 on 2022/12/13.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
